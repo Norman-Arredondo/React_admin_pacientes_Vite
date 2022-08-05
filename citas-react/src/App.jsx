e516e7d5bca860e37b15d37ec5ca65da941cd8bf
Norman Arredondo <arredondo_norman25@outlook.es>
2022-08-04 23:36:08 -0500

    Props
    Pasando información entre componentes con props

--- citas-react/src/App.jsx
+++ citas-react/src/App.jsx
@@ -1,3 +1,4 @@
+import {useState} from 'react'
 import Header from "./components/Header";
 import Formulario from "./components/Formulario";
 import ListadoPacientes from "./components/ListadoPacientes";
@@ -5,9 +6,16 @@ import ListadoPacientes from "./components/ListadoPacientes";
 function App() {
   //Antes de return - para funciones, validaciones
 
+  const [pacientes, setPacientes] = useState([]);
+  const toma1Valor = (valor) => {
+    console.log(valor)
+  }
+
   return (
     <div className="container mx-auto mt-20">
-      <Header />
+      <Header
+        toma1Valor={toma1Valor}  
+      />
 
       <div className="mt-12 md:flex">
           <Formulario />
--- citas-react/src/components/Header.jsx
+++ citas-react/src/components/Header.jsx
@@ -1,7 +1,11 @@
 //Componentes van nombrados con la primer letra en mayúsculas
 //Un componente es una función
 //Function declaration
-function Header() {
+function Header({toma1Valor}) {
+
+  const variableHeader = true;
+  toma1Valor(variableHeader)
+
   return (
     <>
       <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">
